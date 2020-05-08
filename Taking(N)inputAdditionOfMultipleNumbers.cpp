02d8f685ff53b99db862f1f86907014443d4fcc6
Tasneem778 <48061977+Tasneem778@users.noreply.github.com>
2020-05-08 19:17:14 +0600

    Add files via upload

---
+++ Taking(N)inputAdditionOfMultipleNumbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a[10], b=1,c,i,sum=0;
+    cin>>n;
+
+
+    for(i=0;i<n;i++){
+
+        cin>>a[i];
+       sum=sum+a[i];
+
+
+
+}
+ cout<<sum<<endl;
+}
